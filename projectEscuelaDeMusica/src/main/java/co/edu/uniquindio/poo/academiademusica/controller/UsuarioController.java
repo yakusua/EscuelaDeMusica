3efcb2ae92cc1dd23031915b9715f69f6cac3d52
CoderElect <giraldomancilla@gmail.com>
2025-11-15 15:35:35 -0500

    inicio fx

--- projectEscuelaDeMusica/src/main/java/co/edu/uniquindio/poo/academiademusica/controller/UsuarioController.java
+++ projectEscuelaDeMusica/src/main/java/co/edu/uniquindio/poo/academiademusica/controller/UsuarioController.java
@@ -1,4 +1,32 @@
+
 package co.edu.uniquindio.poo.academiademusica.controller;
+import co.edu.uniquindio.poo.academiademusica.model.Academia;
+import co.edu.uniquindio.poo.academiademusica.model.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsuarioController {
+
+    private List<Usuario> listaUsuarios = new ArrayList<>();
+
+    public UsuarioController(Academia academia) {
+    }
+
+    public void agregarUsuario(Usuario usuario) {
+        listaUsuarios.add(usuario);
+    }
+
+    public List<Usuario> obtenerEmpleados() {
+        return listaUsuarios;
+    }
+
+    public boolean eliminarUsuario(String usuario) {
+        listaUsuarios.remove(usuario);
+        return false;
+    }
+
+    public boolean actualizarUsuario(String nombre, String email, String rol) {
+        return false;
+    }
 }
