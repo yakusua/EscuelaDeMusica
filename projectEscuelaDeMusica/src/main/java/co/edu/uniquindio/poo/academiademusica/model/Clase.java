40d82dd03bb46cc050bf0fa6cf94199df0a43b75
CoderElect <giraldomancilla@gmail.com>
2025-11-15 12:42:16 -0500

    gets y sets Clase.java

--- projectEscuelaDeMusica/src/main/java/co/edu/uniquindio/poo/academiademusica/model/Clase.java
+++ projectEscuelaDeMusica/src/main/java/co/edu/uniquindio/poo/academiademusica/model/Clase.java
@@ -28,16 +28,69 @@ public abstract class Clase implements IAsistible {
         this.listAsistencias = new ArrayList<>();
     }
 
-    public String getId() { return id; }
-    public int getNivel() { return nivel; }
-    public LocalDateTime getHorario() { return horario; }
-    public int getCupo() { return cupo; }
-    public Profesor getProfesor() { return profesor; }
-    public Curso getCurso() { return curso; }
-    public Salon getSalon() { return salon; }
+    public String getId() {
+        return id;
+    }
 
-    public void setHorario(LocalDateTime horario) { this.horario = horario; }
-    public void setCupo(int cupo) { this.cupo = cupo; }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public LocalDateTime getHorario() {
+        return horario;
+    }
+
+    public void setHorario(LocalDateTime horario) {
+        this.horario = horario;
+    }
+
+    public int getCupo() {
+        return cupo;
+    }
+
+    public void setCupo(int cupo) {
+        this.cupo = cupo;
+    }
+
+    public Profesor getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(Profesor profesor) {
+        this.profesor = profesor;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public Salon getSalon() {
+        return salon;
+    }
+
+    public void setSalon(Salon salon) {
+        this.salon = salon;
+    }
+
+    public List<Asistencia> getListAsistencias() {
+        return listAsistencias;
+    }
+
+    public void setListAsistencias(List<Asistencia> listAsistencias) {
+        this.listAsistencias = listAsistencias;
+    }
 
     public abstract List<Estudiante> obtenerEstudiantesInscritos();
     protected abstract boolean validarEstudianteInscrito(Estudiante estudiante);
